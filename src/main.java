f8fa6dae7e42d44439a632e740da919f97797fa0
Plexdi <senpai200798@gmail.com>
2024-11-20 15:05:52 +0000

    worked on main.js

--- src/main.java
+++ src/main.java
@@ -9,17 +9,20 @@ public class main{
             int id = 0;
             double amount = 0.00;
             Scanner scanner = new Scanner(System.in);
-            System.out.println("1. Login \n2. Signup");
+            System.out.println("1. Login \n2. Signup \n3. Quit");
             int options = scanner.nextInt();
             scanner.nextLine();
-    
+            
             switch (options) {
                 case 1:
                     System.out.println("Username: ");
                     String username = scanner.nextLine();
                     System.out.println("Password: ");
                     String password = scanner.nextLine();
-                    userManagement.Login(username, password);
+                    boolean validations = userManagement.Login(username, password);
+                    if (validations == true){
+                        System.out.println("1. Deposit \n2. Withdraw \n3. View balance ");
+                    }
                     break;
                 case 2:
                     if (options == 2) {
@@ -27,13 +30,16 @@ public class main{
                         String usernames = scanner.nextLine();       
                         System.out.println("Enter a password: ");
                         String passwords = scanner.nextLine();
-                        userManagement.CreateAccount(usernames, passwords);
+                        boolean validation = userManagement.CreateAccount(usernames, passwords);
+                        if (validation == true){
+                            System.out.println("1. Deposit \n2. Withdraw \n3. View balance ");
+                        }
                     }   break;
                 default:
                     continues = false;
                     break;
             }
-        } while (continues == true);
+        } while (continues == true); 
         
     }
 }
--- src/userManagement.java
+++ src/userManagement.java
@@ -8,46 +8,49 @@ public class UserManagement {
         this.user = new ArrayList<>();
     }
 
-    public void CreateAccount(String username, String password){
+    public boolean CreateAccount(String username, String password){
         int id = 0;
         double amount = 0.00;
         //check if username exists 
         for (Users users : user){
             if (username.equals(users.getUsername())){
                 System.out.println("Username already exists, please try again");
-                return;
+                return false;
             } 
         }
         //check password strength 
         if (password.length() <= 8){
             System.out.println("Password must be at least 8 characters long, please try again ");
-            return;
+            return false;
         }
 
         id +=1;
         Users newUser = new Users(id, username, password, amount);
+        user.add(newUser);
         System.out.println("User " + username + " has added successfully!");
+        return true;
     }
 
-    public void Login(String username, String password) {
+    public boolean Login(String username, String password) {
         if (user.isEmpty()) {
             System.out.println("No users found. Please sign up first.");
-            return;
+            return false;
         }
     
         for (Users eachUsers : user) {
             if (username.equals(eachUsers.getUsername())) {
                 if (password.equals(eachUsers.getPassword())) {
                     System.out.println("Login successful!");
-                    return;
+                    return true;
                 } else {
                     System.out.println("Invalid password!");
-                    return;
+                    return false;
                 }
             }
         }
     
         System.out.println("Invalid username!");
+        return false;
     }
     
 
